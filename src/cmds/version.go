a2326a304a8d8f2a4ddc009ba0796ef0a2a952d4
Josh <josheghani.dev@gmail.com>
2022-02-11 11:43:23 +0330

    update go-artifacts release note

--- src/cmds/version.go
+++ src/cmds/version.go
@@ -11,11 +11,17 @@ func versionCommand() *cobra.Command {
 		Use:   "version",
 		Short: "Print current version of homepi",
 		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Println(fmt.Sprintf("HomePi version: %s", versionInfo.Version))
-			cmd.Println(fmt.Sprintf("Build type: %s", versionInfo.BuildType))
-			cmd.Println(fmt.Sprintf("Build time: %s", versionInfo.BuildTime))
-			cmd.Println(fmt.Sprintf("Golang: %s", versionInfo.GoVersion))
-			cmd.Println(fmt.Sprintf("Compiled by: %s", versionInfo.CompiledBy))
+			fmt.Fprint(
+				cmd.OutOrStdout(),
+				fmt.Sprintf(
+					"%s\n%s\n%s\n%s\n%s",
+					fmt.Sprintf("HomePi version: %s", versionInfo.Version),
+					fmt.Sprintf("Build type: %s", versionInfo.BuildType),
+					fmt.Sprintf("Build time: %s", versionInfo.BuildTime),
+					fmt.Sprintf("Golang: %s", versionInfo.GoVersion),
+					fmt.Sprintf("Compiled by: %s", versionInfo.CompiledBy),
+				),
+			)
 		},
 	}
 }
